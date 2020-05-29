64d2edd8c80f61e3090f3eb2f9215e51e2541b58
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-05-29 12:28:10 +0200

    test: deterministic

--- test/index.js
+++ test/index.js
@@ -1,21 +1,30 @@
 'use strict'
 
-const test = require('ava')
-
+const filenamifyUrl = require('filenamify-url')
+const humanizeUrl = require('humanize-url')
+const existsFile = require('exists-file')
 const getHTML = require('html-get')
+const fs = require('fs').promises
+const path = require('path')
+
+const test = require('ava')
 
 const wappalyzer = require('../src')
 
-test('kikobeats.com', async t => {
-  const { headers, statusCode, url, html } = await getHTML(
-    'https://kikobeats.com'
-  )
-  const result = await wappalyzer({ url, statusCode, html, headers })
-  t.snapshot(result)
-})
+const getFixture = async (url, opts) => {
+  const filename = filenamifyUrl(url)
+  const filepath = path.resolve(__dirname, 'fixtures', filename)
+  const hasFixture = await existsFile(filename)
+  if (hasFixture) return require(filename)
+  const result = await getHTML(url, opts)
+  await fs.writeFile(filepath, JSON.stringify(result, null, 2))
+  return result
+}
 
-test('zeit.co', async t => {
-  const { headers, statusCode, url, html } = await getHTML('https://zeit.co')
-  const result = await wappalyzer({ url, statusCode, html, headers })
-  t.snapshot(result)
+;['https://kikobeats.com', 'https://vercel.com'].forEach(targetUrl => {
+  test(humanizeUrl(targetUrl), async t => {
+    const { headers, statusCode, url, html } = await getFixture(targetUrl)
+    const result = await wappalyzer({ url, statusCode, html, headers })
+    t.snapshot(result)
+  })
 })
